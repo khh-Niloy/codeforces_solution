58d20ca5847f9883769c224c2a73e0edd9b3bd6b
khh-Niloy <niloy.dev.101@gmail.com>
2024-07-27 18:45:43 +0600

    A level - problem 6

---
+++ 100 Problems A/problem_6.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ 
+    string s;
+    cin>>s;
+    
+    for ( char &c : s ) {
+        c = tolower(c);
+    }
+
+    set<char> unique_char(s.begin(), s.end());
+
+    string new_arr(unique_char.begin(), unique_char.end());
+
+    char largest_letter = *max_element(new_arr.begin(), new_arr.end());
+
+    int total_occurance = count(s.begin(), s.end() , largest_letter);
+    
+    for ( int i = 0; i<total_occurance; i++ ) {
+        cout<<largest_letter;
+    }
+
+    return 0;
+}
+
+
+/*Note:
+
+1. max_element -> Return the maximum element in a range.
+It effectively uses ASCII values when comparing characters.
+
+2. count -> Count the number of copies of a value in a sequence.
+*/
