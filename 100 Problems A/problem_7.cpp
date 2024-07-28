37f2b8a1e5417b2edf5193d5b9586ccae37bf1b7
khh-Niloy <niloy.dev.101@gmail.com>
2024-07-28 15:17:53 +0600

    A level - problem 7

---
+++ 100 Problems A/problem_7.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ 
+    int row,col;
+    cin>>row>>col;
+
+    char m[row][col];
+    for ( int i = 0; i<row; i++ ) {
+        for ( int j = 0; j<col; j++ ) {
+            cin>>m[i][j];
+        }
+    }
+
+    // finding row
+    int ROW=1;
+    for ( int i = 0; i<row; i++ ) {
+
+        int count_asterisk = 0;
+
+        for ( int j = 0; j<col; j++ ) {
+            if ( m[i][j] == '*' ) {
+                count_asterisk++;
+            }
+        }
+        if ( count_asterisk == 1 ) {
+            ROW+=i;
+        }
+    }
+
+    // finding column
+    int COL=1;
+    for ( int i = 0; i<col; i++ ) {
+
+        int count_asterisk = 0;
+
+        for ( int j = 0; j<row; j++ ) {
+            if ( m[j][i] == '*' ) {
+                count_asterisk++;
+            }
+        }
+
+        if ( count_asterisk == 1 ) {
+            COL+=i;
+        }
+    }
+
+    cout<<ROW<<" "<<COL<<endl;
+
+    return 0;
+}
+
+
+// Clue:
+
+// 1. Unique * for each row and column
+// 2. Make a rectangle!
