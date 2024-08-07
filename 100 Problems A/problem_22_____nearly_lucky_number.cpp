666ebf3c7f4d5054cb247b751a93cc08a1004616
khh-Niloy <niloy.dev.101@gmail.com>
2024-08-08 00:11:00 +0600

    problem 22

---
+++ 100 Problems A/problem_22_____nearly_lucky_number.cpp
@@ -0,0 +1,30 @@
+/*
+Problem 22: Nearly Lucky Number
+Problem Link: https://codeforces.com/contest/110/problem/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ 
+    string number;
+    cin>>number;
+    
+    int count = 0;
+
+    for ( int i = 0; i<number.length(); i++ ) {
+        if ( number[i] == '4' || number[i] == '7' ) {
+            count++;
+        }
+    }
+
+    if ( count == 4 || count == 7 ) {
+        cout<<"YES";
+    }
+    else {
+        cout<<"NO";
+    }
+
+    return 0;
+}
