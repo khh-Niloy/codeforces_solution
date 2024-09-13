1d18512076364dd8e840155cab60a5bba436fc6f
khh-Niloy <niloy.dev.101@gmail.com>
2024-09-13 18:43:51 +0600

    added 65

---
+++ 100 Problems A/problem_65_____again_twenty_five!.cpp
@@ -0,0 +1,16 @@
+/* 
+Problem 65: Again Twenty Five!
+Problem Link: https://codeforces.com/problemset/problem/630/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    
+    int n;
+    cin>>n;
+    cout<<"25";
+
+    return 0;
+}
